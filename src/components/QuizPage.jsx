964c3b29fcb630cd9606885cc91e26d9865e2768
Michaelhybrid <michaeloludare15@gmail.com>
2022-12-01 00:46:01 +0100

    fom

---
+++ src/components/QuizPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const QuizPage = () => {
+  return (
+    <div className="QuizPage">
+        <div className="intro__page">
+          <h1 className="intro__page__title">Quizzical</h1>
+          <a href="#" className="intro__page__paragraph">Some description if needed</a>
+          <button className="intro__page__button">Start quiz</button>
+        </div>
+    </div>
+  )
+}
+
+export default QuizPage
