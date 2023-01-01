0ab2d98d7b087688e439e0529d743b94fb0ebdc8
Michaelhybrid <michaeloludare15@gmail.com>
2023-01-01 23:11:36 +0100

    new year

--- src/components/QuestionsPage.jsx
+++ src/components/QuestionsPage.jsx
@@ -60,7 +60,7 @@ const QuestionsPage = (props) => {
         <Loading background="rgb(41,50,100)" />
       ) : (
         <form>
-          {score === 5 ? <Confetti width={width} height={height} style={{zIndex: 10}}/> : <></>}
+          {score < 5 ? <Confetti width={width} height={height} style={{zIndex: 10}}/> : <></>}
             {/* FIRST QUESTION */}
         <div className="question__page">
               <h4>{decode(questions[0].question)}</h4>
